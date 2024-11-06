530cad7889a97e046198901d6b44f11ef084f51d
Rahmankhattak711 <rahmankhattak796@gmail.com>
2024-11-06 17:11:17 +0500

    color changes

--- src/app/component/Button.tsx
+++ src/app/component/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 }
 const Button = ({ onClick, text, className ,type}: ButtonProps) => {
   return (
-    <button type={type} onClick={onClick} className={`${className} mt-4 mb-4 rounded-sm py-2 px-4 bg-gray-400 text-white`}>
+    <button type={type} onClick={onClick} className={`${className} mt-4 mb-4 rounded-sm py-2 px-4 text-white`}>
       {text}
     </button>
   );
